7142e898119734cc61c7ffada60f3460148a3c2e
Habib Khairi dzaki <khairiDzaki>
2026-09-17 17:12:56 +0700

    index ada yang diganti

--- projek3/index.php
+++ projek3/index.php
@@ -136,7 +136,7 @@ $role = $_SESSION['role'];
             </p>
 
             <p>
-                Anda login sebagai 
+                Anda login sebagai aaa
                 <strong><?= htmlspecialchars(ucfirst($role)); ?></strong>.
             </p>
 
